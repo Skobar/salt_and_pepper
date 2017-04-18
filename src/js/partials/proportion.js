acb0b8c39ba33bd4635f6e4b11e1a7bd9ce9b921
RUSICH <artem.skobar@yandex.ru>
2017-04-18 23:52:44 +0300

    Анимация среднего контент блока

--- src/js/partials/proportion.js
+++ src/js/partials/proportion.js
@@ -1,26 +1,28 @@
 
-function setHeight( el, i ){
+function setHeight( el, i){
 	var elem 		= el;
 	var index 		= i;
 	var elem_width 	= elem.width();
 	elem.height( elem_width / index );
-	// console.log(elem.attr("class") + ": " + elem_width / index);
 }
 
 $(document).ready(function(){
-	var header 				= $(".animation__header");
+	var header 				= $(".header--animation");
 	var top_content_left 	= $(".top-content--billboard");
+	var top_content_gallery = $(".top-content--gallery");
 	var middle_content_left = $(".middle-content--billboard");
 	var bottom_content_left = $(".bottom-content--billboard");
 
 	setHeight( header, 2.1559633 );
-	setHeight( top_content_left, 0.9 );
+	setHeight( top_content_left, 0.97 );
+	setHeight( top_content_gallery, 0.99 );
 	setHeight( middle_content_left, 0.987 );
 	setHeight( bottom_content_left, 0.983 );
 
 	$(window).resize(function(){
 		setHeight( header, 2.1559633 );
-		setHeight( top_content_left, 0.9 );
+		setHeight( top_content_left, 0.97 );
+		setHeight( top_content_gallery, 0.99 );
 		setHeight( middle_content_left, 0.987 );
 		setHeight( bottom_content_left, 0.983 );
 	});
