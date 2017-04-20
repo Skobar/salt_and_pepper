88948bc48b75a55a76d309ae5a5c963a7f42cab0
RUSICH <artem.skobar@yandex.ru>
2017-04-21 00:19:16 +0300

    Начал респонсивное меню

--- src/js/partials/animation.js
+++ src/js/partials/animation.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
-
+	// middle and bottom blocks animation on
 	var middle_content = $(".middle-content--billboard");
 	var middle_gallery = $(".middle-content--gallery");
 	var bottom_content = $(".bottom-content--billboard");
 	var bottom_gallery = $(".bottom-content--gallery");
+	var win_h = $(window).height();
 
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > (middle_content.offset().top - 300)) {
+		if ($(this).scrollTop() > (middle_content.offset().top - win_h)) {
 			middle_content.addClass("animation__to-right");
 			middle_gallery.addClass("animation__to-left");
 		}
 
-		if($(this).scrollTop() > (bottom_content.offset().top - 300)){
+		if($(this).scrollTop() > (bottom_content.offset().top - win_h)){
 			bottom_content.addClass("animation__to-left");
 			bottom_gallery.addClass("animation__to-right");
 		}
+	});
+
+
+	// mobile sidebar menu animation
+	var sidebar_close = $(".sidebar-close");
+	var menu_item = $(".sidebar-menu--item");
+
+	sidebar_close.on("click", function(){
+	  sidebar_close.toggleClass("active");
+	  menu_item.toggleClass("sidebar-menu--item__active");
 	});
 });
